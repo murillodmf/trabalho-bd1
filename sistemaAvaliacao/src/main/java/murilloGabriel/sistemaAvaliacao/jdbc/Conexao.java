00f0abdbe0865bb2d0197f70b79ccd35f86036af
Murillo <murillo.ferraz@uel.br>
2025-06-23 19:35:46 -0300

    conexao

--- sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/jdbc/Conexao.java
+++ sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/jdbc/Conexao.java
@@ -6,20 +6,5 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/seu_banco";
-    private static final String USER = "seu_usuario";
-    private static final String PASSWORD = "sua_senha";
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.err.println("Erro ao carregar driver PostgreSQL: " + ex.getMessage());
-        }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
 } 
     
