6bb387b26dfbc6c24a9218ae23708c5f855993f0
Murillo <murillo.ferraz@uel.br>
2025-06-23 17:13:41 -0300

    projetos em spring

---
+++ sistemaAvaliacao/src/test/java/murilloGabriel/sistemaAvaliacao/SistemaAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package murilloGabriel.sistemaAvaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
