173f5e7baa595d25842981759773938266bc75f0
Murillo <murillo.ferraz@uel.br>
2025-06-25 20:16:04 -0300

    correcao em alguns models

--- sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Objetiva.java
+++ sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Objetiva.java
@@ -1,38 +1,35 @@
 package murilloGabriel.sistemaAvaliacao.model;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
-@Table("objetiva")
+import java.util.List;
+
 public class Objetiva {
-    @Id
-    @Column("id_questao")
+
     private Integer idQuestao;
-    @Column("alternativa_a")
-    private String alternativaA;
-    @Column("alternativa_b")
-    private String alternativaB;
-    @Column("alternativa_c")
-    private String alternativaC;
-    @Column("alternativa_d")
-    private String alternativaD;
-    @Column("alternativa_e")
-    private String alternativaE;
-    @Column("resposta_correta")
     private String respostaCorreta;
-    // Getters e Setters...
-    public Integer getIdQuestao() { return idQuestao; }
-    public void setIdQuestao(Integer idQuestao) { this.idQuestao = idQuestao; }
-    public String getAlternativaA() { return alternativaA; }
-    public void setAlternativaA(String alternativaA) { this.alternativaA = alternativaA; }
-    public String getAlternativaB() { return alternativaB; }
-    public void setAlternativaB(String alternativaB) { this.alternativaB = alternativaB; }
-    public String getAlternativaC() { return alternativaC; }
-    public void setAlternativaC(String alternativaC) { this.alternativaC = alternativaC; }
-    public String getAlternativaD() { return alternativaD; }
-    public void setAlternativaD(String alternativaD) { this.alternativaD = alternativaD; }
-    public String getAlternativaE() { return alternativaE; }
-    public void setAlternativaE(String alternativaE) { this.alternativaE = alternativaE; }
-    public String getRespostaCorreta() { return respostaCorreta; }
-    public void setRespostaCorreta(String respostaCorreta) { this.respostaCorreta = respostaCorreta; }
+    private List<String> alternativas;
+
+    public Integer getIdQuestao() {
+        return idQuestao;
+    }
+
+    public void setIdQuestao(Integer idQuestao) {
+        this.idQuestao = idQuestao;
+    }
+
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public void setRespostaCorreta(String respostaCorreta) {
+        this.respostaCorreta = respostaCorreta;
+    }
+
+    public List<String> getAlternativas() {
+        return alternativas;
+    }
+
+    public void setAlternativas(List<String> alternativas) {
+        this.alternativas = alternativas;
+    
+    }
 }
--- sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Resposta.java
+++ sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Resposta.java
@@ -1,45 +1,50 @@
 package murilloGabriel.sistemaAvaliacao.model;
 
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
-// Nota: Não usamos @Id aqui porque a chave primária é composta.
-// O acesso será feito pelo repositório usando os três campos da chave.
-@Table("resposta")
 public class Resposta {
 
-    @Column("id_prova")
     private Integer idProva;
-
-    @Column("id_questao")
     private Integer idQuestao;
-
-    @Column("matricula_aluno")
-    private Integer matriculaAluno;
-
+    private Integer matricula;
     private Double nota;
     private String comentario;
 
-    @Column("resposta_objetiva")
-    private String respostaObjetiva;
+    public Integer getIdProva() {
+        return idProva;
+    }
 
-    @Column("resposta_dissertativa")
-    private String respostaDissertativa;
+    public void setIdProva(Integer idProva) {
+        this.idProva = idProva;
+    }
 
-    // Getters e Setters...
-    public Integer getIdProva() { return idProva; }
-    public void setIdProva(Integer idProva) { this.idProva = idProva; }
-    public Integer getIdQuestao() { return idQuestao; }
-    public void setIdQuestao(Integer idQuestao) { this.idQuestao = idQuestao; }
-    public Integer getMatriculaAluno() { return matriculaAluno; }
-    public void setMatriculaAluno(Integer matriculaAluno) { this.matriculaAluno = matriculaAluno; }
-    public Double getNota() { return nota; }
-    public void setNota(Double nota) { this.nota = nota; }
-    public String getComentario() { return comentario; }
-    public void setComentario(String comentario) { this.comentario = comentario; }
-    public String getRespostaObjetiva() { return respostaObjetiva; }
-    public void setRespostaObjetiva(String respostaObjetiva) { this.respostaObjetiva = respostaObjetiva; }
-    public String getRespostaDissertativa() { return respostaDissertativa; }
-    public void setRespostaDissertativa(String respostaDissertativa) { this.respostaDissertativa = respostaDissertativa; }
+    public Integer getIdQuestao() {
+        return idQuestao;
+    }
+
+    public void setIdQuestao(Integer idQuestao) {
+        this.idQuestao = idQuestao;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
-
