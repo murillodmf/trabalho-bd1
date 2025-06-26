0578b225101f5bbe8bb291ef3275bedbfc036a09
Gabriel <gabriel.navarro7@uel.br>
2025-06-26 12:58:10 -0300

    Atualização Classe Professor

--- sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Professor.java
+++ sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/model/Professor.java
@@ -3,24 +3,10 @@ package murilloGabriel.sistemaAvaliacao.model;
 public class Professor {
 
     private Integer registro;
-    private String nome;
     private String cpf;
-
-    public Integer getRegistro() {
-        return registro;
-    }
-
-    public void setRegistro(Integer registro) {
-        this.registro = registro;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    private String pnome;
+    private String snome;
+    private Integer idade;
 
     public String getCpf() {
         return cpf;
@@ -28,5 +14,29 @@ public class Professor {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public String getPnome() {
+        return pnome;
+    }
+
+    public void setPnome(String pnome) {
+        this.pnome = pnome;
+    }
+
+    public String getSnome() {
+        return snome;
+    }
+
+    public void setSnome(String snome) {
+        this.snome = snome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
     }
 }
