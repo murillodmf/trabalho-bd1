e98ff5feba225b8eb4a804d43a883114167d002f
Murillo <murillo.ferraz@uel.br>
2025-06-26 11:04:53 -0300

    correcao

--- sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/repository/AvaliacaoRepository.java
+++ sistemaAvaliacao/src/main/java/murilloGabriel/sistemaAvaliacao/repository/AvaliacaoRepository.java
@@ -4,9 +4,6 @@ import murilloGabriel.sistemaAvaliacao.model.Avaliacao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
