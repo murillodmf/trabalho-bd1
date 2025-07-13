ee90c87b86bae09cf5b9c4ef65901b0f296c38af
Gabriel <gabriel.navarro7@uel.br>
2025-07-12 23:30:33 -0300

    Atualização NavBar

--- sistema-frontend/src/components/Navbar.js
+++ sistema-frontend/src/components/Navbar.js
@@ -1,23 +1,25 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">Sistema de Avaliação</Link>
-      </div>
-      <div className="navbar-links">
-        <Link to="/alunos">Alunos</Link>
-        <Link to="/professores">Professores</Link>
-        <Link to="/turmas">Turmas</Link>
-        <Link to="/questoes">Questões</Link>
-        <Link to="/avaliacoes">Avaliações</Link>
-        <Link to="/aluno-turma">Matriculas</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">Sistema de Avaliação</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/alunos">Alunos</Link>
+                <Link to="/professores">Professores</Link>
+                <Link to="/turmas">Turmas</Link>
+                <Link to="/questoes">Questões</Link>
+                <Link to="/avaliacoes">Avaliações</Link>
+                <Link to="/aluno-turma">Matriculas</Link>
+                <Link to="/relatorios">Relatórios</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
